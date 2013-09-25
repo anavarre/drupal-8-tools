bf1cdd2145c5471167aaaa276d5959d7693892a3
Aurelien Navarre <aurelien.navarre@gmail.com>
2013-09-25 11:24:23 +0200

    Standardized variables

--- setup/delete.sh
+++ setup/delete.sh
@@ -3,40 +3,40 @@
 # Invoke the script from anywhere (e.g system alias)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/common
+source ${DIR}/common
 
-echo -e $RED_START"############################################################"$RED_END
-echo -e $RED_START"# WARNING! You're about to delete a site and all its data! #"$RED_END
-echo -e $RED_START"############################################################"$RED_END
+echo -e "${RED_START}############################################################${RED_END}"
+echo -e "${RED_START}# WARNING! You're about to delete a site and all its data! #${RED_END}"
+echo -e "${RED_START}############################################################${RED_END}"
 
 echo -e "Which Drupal docroot should we delete?"
 read SITE
 
 read -p "Are you sure? [Y/N] "
-if [[ $REPLY =~ ^[Nn]$ ]]
+if [[ ${REPLY} =~ ^[Nn]$ ]]
 then
-	echo -e $GREEN_START"Back to the comfort zone. Aborting."$GREEN_END
+	echo -e "${GREEN_START}Back to the comfort zone. Aborting.{$GREEN_END}"
 	exit 0
 fi
 
 # Docroot exists
-if [[ ! -d $WEBROOT/$SITE ]]; then
-	echo -e $GREEN_START"The $SITE docroot doesn't exist! Aborting."$GREEN_END
+if [[ ! -d ${WEBROOT}/${SITE} ]]; then
+	echo -e "${GREEN_START}The ${SITE} docroot doesn't exist! Aborting.${GREEN_END}"
 	exit 0
 fi
 
 echo -e "\tDeleting Drupal docroot..."
-	rm -Rf $WEBROOT/$SITE
+	rm -Rf ${WEBROOT}/${SITE}
 
 echo -e "\tDeleting Apache vHost..."
-	a2dissite $SITE > /dev/null 2>&1
+	a2dissite ${SITE} > /dev/null 2>&1
 	service apache2 reload > /dev/null 2>&1
-	rm -f $SITES_AVAILABLE/$SITE
+	rm -f ${SITES_AVAILABLE}/${SITE}
 	
 echo -e "\tDeleting hosts file entry..."
-	sed -i "/$SITE.$SUFFIX/d" /etc/hosts
+	sed -i "/${SITE}.${SUFFIX}/d" /etc/hosts
 
 echo -e "\tDeleting database..."
-	$MYSQL -uroot -proot -e "DROP DATABASE IF EXISTS $SITE"
+	${MYSQL} -uroot -proot -e "DROP DATABASE IF EXISTS $SITE"
 
-echo -e $GREEN_START"Successfully removed http://$SITE.$SUFFIX"$GREEN_END
+echo -e "${GREEN_START}Successfully removed http://${SITE}.${SUFFIX}${GREEN_END}"
--- setup/install.sh
+++ setup/install.sh
@@ -3,7 +3,7 @@
 # Invoke the script from anywhere (e.g system alias)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-source $DIR/common
+source ${DIR}/common
 
 ################
 # REQUIREMENTS #
@@ -20,7 +20,7 @@ fi
 for pkg in $PHP; do
 	if ! dpkg --get-selections | grep -q "^$pkg[[:space:]]*install$" >/dev/null; then
 		echo -e "\tInstalling $pkg..."
-		apt-get -qq install $pkg
+		apt-get -qq install ${pkg}
 	fi
 done
 
@@ -31,47 +31,47 @@ echo -e "What should be the name of the new Drupal docroot?"
 read SITE
 
 # Docroot exists
-if [[ -d $WEBROOT/$SITE ]]; then
-	echo -e $RED_START"docroot already exists! Aborting."$RED_END
+if [[ -d ${WEBROOT}/${SITE} ]]; then
+	echo -e "${RED_START}docroot already exists! Aborting.${RED_END}"
 	exit 0
 fi
 
 # Download archive only if needed
-if [[ ! -f $TMP/$DRUPAL ]]; then
-	echo "Downloading $DRUPAL..."
-	wget -P $TMP -q http://ftp.drupal.org/files/projects/$DRUPAL
+if [[ ! -f ${TMP}/${DRUPAL} ]]; then
+	echo "Downloading ${DRUPAL}..."
+	wget -P ${TMP} -q http://ftp.drupal.org/files/projects/${DRUPAL}
 fi
 
-echo "Unpacking $DRUPAL..."
-tar -C $WEBROOT -xzf $TMP/$DRUPAL
+echo "Unpacking ${DRUPAL}..."
+tar -C ${WEBROOT} -xzf ${TMP}/${DRUPAL}
 
-mv $WEBROOT/$RELEASE $WEBROOT/$SITE
-echo -e $GREEN_START"Successfully created Drupal docroot: $SITE"$GREEN_END
+mv ${WEBROOT}/${RELEASE} ${WEBROOT}/${SITE}
+echo -e "${GREEN_START}Successfully created Drupal docroot under ${WEBROOT}/${SITE}${GREEN_END}"
 
 echo -e "\tCopying settings.php file..."
-cp $WEBROOT/$SITE/sites/default/default.settings.php $WEBROOT/$SITE/sites/default/settings.php
+cp ${WEBROOT}/${SITE}/sites/default/default.settings.php ${WEBROOT}/${SITE}/sites/default/settings.php
 
 echo -e "\tSetting correct permissions..."
 # Allows to create the files and translations dirs automatically
-chmod a+w $WEBROOT/$SITE/sites/default
-chmod a+w $WEBROOT/$SITE/sites/default/settings.php  
+chmod a+w ${WEBROOT}/${SITE}/sites/default
+chmod a+w ${WEBROOT}/${SITE}/sites/default/settings.php  
 
 # Apache setup
 echo -e "\tProvisionning Apache vhost..."
-	cp $SITES_AVAILABLE/default $SITES_AVAILABLE/$SITE
+	cp ${SITES_AVAILABLE}/default ${SITES_AVAILABLE}/${SITE}
 	# Adding ServerName directive
-	sed -i "3i\\\tServerName $SITE.$SUFFIX" $SITES_AVAILABLE/$SITE
+	sed -i "3i\\\tServerName ${SITE}.${SUFFIX}" ${SITES_AVAILABLE}/${SITE}
 	# Modifying directives
-	sed -i "s:/var/www:/var/www/html/$SITE:g" $SITES_AVAILABLE/$SITE
+	sed -i "s:/var/www:/var/www/html/${SITE}:g" ${SITES_AVAILABLE}/${SITE}
 	# Make sure that Drupal's .htaccess clean URLs will work fine
-	sed -i "s/AllowOverride None/AllowOverride All/g" $SITES_AVAILABLE/$SITE
+	sed -i "s/AllowOverride None/AllowOverride All/g" ${SITES_AVAILABLE}/${SITE}
 
 echo -e "\tEnabling site..."
-	a2ensite $SITE > /dev/null 2>&1
+	a2ensite ${SITE} > /dev/null 2>&1
 	service apache2 reload > /dev/null 2>&1
 
 echo -e "\tAdding hosts file entry..."
-	sed -i "1i127.0.0.1\t$SITE.$SUFFIX" /etc/hosts
+	sed -i "1i127.0.0.1\t${SITE}.${SUFFIX}" /etc/hosts
 
 # MySQL queries
 DB_CREATE="CREATE DATABASE IF NOT EXISTS $SITE DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci"
@@ -79,9 +79,9 @@ DB_PERMS="GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK
 SQL="${DB_CREATE};${DB_PERMS}"
 
 echo -e "\tCreating MySQL database..."
-	$MYSQL -uroot -proot -e "$SQL"
+	$MYSQL -uroot -proot -e "${SQL}"
 
-echo -e $GREEN_START"Site is available at http://$SITE.$SUFFIX"$GREEN_END
+echo -e "${GREEN_START}Site is available at http://${SITE}.${SUFFIX}${GREEN_END}"
 
 # echo -e "Reverting permissions for security reasons..."
 # chmod go-w sites/default
