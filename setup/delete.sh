acbc36d0653149bc60afc52c56c1a72783cfe8bd
Aurelien Navarre <aurelien.navarre@gmail.com>
2013-10-07 19:51:45 +0200

    Refactored site install prompt

--- setup/delete.sh
+++ setup/delete.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Invoke the script from anywhere (e.g system alias)
+# Invoke the script from anywhere (e.g .bashrc alias)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+source ${DIR}/common
+
 if [ "$(whoami)" != "root" ]; then
-	echo "You are required to run this script as root or with sudo...aborting."
+	echo -e "${RED_START}You are required to run this script as root or with sudo! Aborting...${RED_END}"
 	exit 1
 fi
-
-source ${DIR}/common
 
 echo -e "${RED_START}############################################################${RED_END}"
 echo -e "${RED_START}# WARNING! You're about to delete a site and all its data! #${RED_END}"
 echo -e "${RED_START}############################################################${RED_END}"
 
-echo -e "Which Drupal docroot should we delete?"
-read SITE
+SITENAME=$1
+if [ -z $1 ]; then
+	echo -n "Which Drupal docroot should we delete? "
+	read SITENAME
+fi
 
 read -p "Are you sure? [Y/N] "
 if [[ ${REPLY} =~ ^[Nn]$ ]]
@@ -25,23 +28,23 @@ then
 fi
 
 # Docroot exists
-if [[ ! -d ${WEBROOT}/${SITE} ]]; then
-	echo -e "${GREEN_START}The ${SITE} docroot doesn't exist! Aborting.${GREEN_END}"
+if [[ ! -d ${WEBROOT}/${SITENAME} ]]; then
+	echo -e "${GREEN_START}The ${SITENAME} docroot doesn't exist! Aborting.${GREEN_END}"
 	exit 0
 fi
 
 echo -e "\tDeleting Drupal docroot..."
-	rm -Rf ${WEBROOT}/${SITE}
+	rm -Rf ${WEBROOT}/${SITENAME}
 
 echo -e "\tDeleting Apache vHost..."
-	a2dissite ${SITE} > /dev/null 2>&1
+	a2dissite ${SITENAME} > /dev/null 2>&1
 	service apache2 reload > /dev/null 2>&1
-	rm -f ${SITES_AVAILABLE}/${SITE}
+	rm -f ${SITES_AVAILABLE}/${SITENAME}
 	
 echo -e "\tDeleting hosts file entry..."
-	sed -i "/${SITE}.${SUFFIX}/d" /etc/hosts
+	sed -i "/${SITENAME}.${SUFFIX}/d" /etc/hosts
 
 echo -e "\tDeleting database..."
-	${MYSQL} -uroot -proot -e "DROP DATABASE IF EXISTS $SITE"
+	${MYSQL} -uroot -proot -e "DROP DATABASE IF EXISTS $SITENAME"
 
-echo -e "${GREEN_START}Successfully removed http://${SITE}.${SUFFIX}${GREEN_END}"
+echo -e "${GREEN_START}Successfully removed http://${SITENAME}.${SUFFIX}${GREEN_END}"
