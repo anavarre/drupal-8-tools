24f8628d84094c2207f1e9c29f4bc3e57b059175
Aurelien Navarre <aurelien.navarre@gmail.com>
2013-11-12 18:22:39 +0100

    Add more comments

--- cmi/dump.sh
+++ cmi/dump.sh
@@ -12,11 +12,11 @@ if [[ "$(whoami)" != "root" ]]; then
 	exit 1
 fi
 
-# List all existing docroots
+# Docroot selection
 echo -e "${BLUE}What Drupal docroot should we work with?${COLOR_ENDING} "
 cd ${WEBROOT}
 
-# Docroot selection
+# List all existing docroots
 PS3="Enter docroot number: "
 CODEBASE=(`ls -d */ | sed s:/::g;`)
 select DOCROOT in "${CODEBASE[@]}"; do
@@ -29,11 +29,11 @@ done
 
 echo -e "${GREEN}Docroot is: ${DOCROOT}${COLOR_ENDING}"
 
-# List all existing sites
+# (Multi)site selection
 echo -e "${BLUE}What site should we trigger a backup for?${COLOR_ENDING} "
 cd ${WEBROOT}/${DOCROOT}/sites/
 
-# Site selection
+# List all existing (multi)sites
 PS3="Enter site number: "
 DIR=(`ls -d */ | sed s:/::g;`)
 select SITE in "${DIR[@]}"; do
@@ -46,7 +46,7 @@ done
 
 echo -e "${GREEN}Site is: ${SITE}${COLOR_ENDING}"
 
-# Backup dir exists
+# Create backup dir if needed
 if [[ ! -d ${WEBROOT}/${DOCROOT}/backup ]]; then
 	echo -e "\t Creating backup directory..."
 	mkdir ${WEBROOT}/${DOCROOT}/backup
@@ -55,9 +55,10 @@ else
 	echo -e "Backup directory already exists. Ignoring..."
 fi
 
+# Database selection
 echo -e "${BLUE}What database should we create a MySQL dump for?${COLOR_ENDING} "
 
-# Database selection
+# List all existing databases
 PS3="Enter database number: "
 SQL=$(mysql -u root -proot -e "SHOW DATABASES;")
 DATABASES=( $( for DB in ${SQL} ; do echo ${DB} | egrep -v "(test|*_schema|Database)" ; done ) )
@@ -65,21 +66,26 @@ select DB in "${DATABASES[@]}"; do
 	break;
 done
 
-# Creating on-demand backup dir
+echo -e "${GREEN}Database is: ${DB}${COLOR_ENDING}"
+
+# Start backup operations
+echo -e "${BLUE}Starting backup...${COLOR_ENDING} "
+
+# Create on-demand backup dir and add timestamp
 ONDEMAND=BACKUP_${DB}_${NOW}
 mkdir ${WEBROOT}/${DOCROOT}/backup/${ONDEMAND}
 
-# MySQL site backup
+# Trigger MySQL site backup
 mysqldump -u root -proot ${DB} > ${WEBROOT}/${DOCROOT}/backup/${ONDEMAND}/${DB}.sql
 
 # Determine CMI's config dir.
 cd ${WEBROOT}/${DOCROOT}/sites/${SITE}/files/
 CONFIG=`(find . -maxdepth 1 -type d -name "config_*" | sed 's/^.\{2\}//')`
 
-# CMI backup
+# Trigger CMI files backup
 cp -R ${WEBROOT}/${DOCROOT}/sites/${SITE}/files/${CONFIG}/active/ ${WEBROOT}/${DOCROOT}/backup/${ONDEMAND}/
 
-# Compressing data
+# Compress data
 cd ${WEBROOT}/${DOCROOT}/backup/
 tar -zcf ${ONDEMAND}.tar.gz ${ONDEMAND}/
 rm -Rf ${ONDEMAND}/
