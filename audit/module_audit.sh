1e331b3dd70fb2650bf38fb820bacf338d618d25
Aurelien Navarre <aurelien.navarre@gmail.com>
2014-04-08 14:52:01 +0200

    Add more FAPI functions

--- audit/module_audit.sh
+++ audit/module_audit.sh
@@ -26,20 +26,27 @@ FUNCTIONS=(
   [drupal_form_submit]="\Drupal::formBuilder()->submitForm()"
   [drupal_get_form]="\Drupal::formBuilder()->getForm()"
   [drupal_goto]="$this->redirect($route_name);"
-  [drupal_process_form]="\Drupal::formBuilder()->processForm()"
   [drupal_prepare_form]="\Drupal::formBuilder()->prepareForm()"
+  [drupal_process_form]="\Drupal::formBuilder()->processForm()"
+  [drupal_rebuild_form]="\Drupal::formBuilder()->rebuildForm()"
+  [drupal_redirect_form]="\Drupal::formBuilder()->redirectForm()"
+  [drupal_retrieve_form]="\Drupal::formBuilder()->retrieveForm()"
+  [drupal_validate_form]="\Drupal::formBuilder()->validateForm()"
+  [form_clear_error]="\Drupal::formBuilder()->clearErrors()"
+  [form_get_cache]="\Drupal::formBuilder()->getCache()"
+  [form_get_errors]="\Drupal::formBuilder()->getErrors()"
+  [form_get_error]="\Drupal::formBuilder()->getError()"
+  [form_error]="\Drupal::formBuilder()->setError()"
+  [form_execute_handlers]="\Drupal::formBuilder()->executeHandlers()"
+  [form_set_cache]="\Drupal::formBuilder()->setCache()"
+  [form_set_error]="\Drupal::formBuilder()->setErrorByName()"
+  [form_state_defaults]="\Drupal::formBuilder()->getFormStateDefaults()"
 )
 
 echo -e "${BLUE}Auditing functions...${COLOR_ENDING}"
 
-for VALUES in ${!FUNCTIONS[@]}; do
-  keyref="queue${VALUES}[@]"
-
-  for API_REF in ${!FUNCTIONS[@]}; do
-    if [[ $(find ${VALID_PATH} -type f | xargs grep --color=auto "${API_REF}") ]]; then
-      echo -e "\tUpdate ${RED}${API_REF}${COLOR_ENDING} with ${GREEN}${FUNCTIONS[@]}${COLOR_ENDING}"
-    fi
-  done
-
+for API_REF in ${!FUNCTIONS[@]}; do
+  if [[ $(find ${VALID_PATH} -type f | xargs grep "${API_REF}") ]]; then
+    echo -e "\tUpdate ${RED}${API_REF}${COLOR_ENDING} with ${GREEN}${FUNCTIONS[@]}${COLOR_ENDING}"
+  fi
 done
-
