c3280ca86e20b661c96277b5488576e56d30298b
Aurelien Navarre <aurelien.navarre@gmail.com>
2014-04-11 17:21:04 +0200

    Add more functions and support for Superglobals

--- audit/module_audit.sh
+++ audit/module_audit.sh
@@ -54,6 +54,7 @@ FUNCTIONS=(
   [hook_date_formats]="system.date_format.{string}.yml"
   [hook_date_formats_alter]="system.date_format.{string}.yml"
   [hook_date_format_types]="system.date_format.{string}.yml"
+  [hook_library_info]="{module}.libraries.yml"
   [l]="\$this->l('title', \$route_name);"
   [language]="LanguageManagerInterface::getCurrentLanguage"
   [language_default]="LanguageManagerInterface::getDefaultLanguage"
@@ -88,12 +89,15 @@ FUNCTIONS=(
   [language_switcher_session]="LanguageNegotiationSession::getLanguageSwitchLinks"
   [language_switcher_url]="LanguageNegotiationFoo::getLanguageSwitchLinks"
   [language_url_rewrite_session]="LanguageNegotiationSession::processOutbound"
+  [list_themes]="\$theme_handler = \Drupal::service('theme_handler');"
   [module_exists]="\$this->moduleHandler()->moduleExists(\$m);"
   [st]="TODO"
   [system_get_date_format]="Drupal 8's Configuration API"
   [system_get_date_formats]="Drupal 8's Configuration API"
   [system_get_date_types]="Drupal 8's Configuration API"
   [t]="\$this->t('some text');"
+  [theme_enable]="\$theme_handler->enable(\$theme_list);"
+  [theme_disable]="\$theme_handler->disable(\$theme_list);"
   [theme_link]="#type' => 'link'"
   [url]="\$this->url(\$route_name);"
   [user_access]="User::hasPermission()"
@@ -101,8 +105,28 @@ FUNCTIONS=(
 
 echo -e "${BLUE}Auditing functions...${COLOR_ENDING}"
 
-for API_REF in ${!FUNCTIONS[@]}; do
-  if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep -s -E " ${API_REF}[(*]") ]]; then
-    echo -e "\tReplace ${RED}${API_REF}()${COLOR_ENDING} by ${GREEN}${FUNCTIONS[${API_REF}]}${COLOR_ENDING}"
+for API_FUNCTIONS in ${!FUNCTIONS[@]}; do
+  if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep -s -E " ${API_FUNCTIONS}[(*$]") ]]; then
+    echo -e "\tReplace ${RED}${API_FUNCTIONS}()${COLOR_ENDING} by ${GREEN}${FUNCTIONS[${API_FUNCTIONS}]}${COLOR_ENDING}"
   fi
 done
+
+echo -e "${BLUE}Auditing superglobals...${COLOR_ENDING}"
+
+# Superglobals array.
+declare -A SUPERGLOBALS
+SUPERGLOBALS=(
+  [\$_POST]="TODO"
+  [\$_SERVER]="TODO"
+)
+
+# Searching for $GET_['q'] doesn't work well as an array key. Hardcoding it for now.
+if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep "\$_GET\['q']") ]]; then
+  echo -e "\tReplace ${RED}\$_GET['q']${COLOR_ENDING} by ${GREEN}current_path()${COLOR_ENDING}"
+fi
+
+for API_SGLOBALS in ${!SUPERGLOBALS[@]}; do
+  if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep "${API_SGLOBALS}") ]]; then
+    echo -e "\tReplace ${RED}${API_SGLOBALS}()${COLOR_ENDING} by ${GREEN}${SUPERGLOBALS[${API_SGLOBALS}]}${COLOR_ENDING}"
+  fi
+done 
