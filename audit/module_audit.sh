f4b1927bfb0cdd6d6e80a700ff28be81dcf5e5eb
Aurelien Navarre <aurelien.navarre@gmail.com>
2015-01-14 15:52:48 +0100

    Add more functions and support for globals

--- audit/module_audit.sh
+++ audit/module_audit.sh
@@ -47,6 +47,7 @@ FUNCTIONS=(
   [comment_new_page_count]="CommentStorage::getNewCommentPageNumber()"
   [comment_num_new]="CommentManager::getCountNewComments(\$entity, \$field_name, \$timestamp)"
   [current_path]="Replaced by the <current> route - See https://www.drupal.org/node/2382211"
+  [decode_entities]="String::decodeEntities()"
   [drupal_add_css]="drupal_process_attached()"
   [drupal_add_http_header]="\$response->headers->set(\$name, \$value)"
   [drupal_add_js]="drupal_process_attached()"
@@ -54,6 +55,7 @@ FUNCTIONS=(
   [drupal_alter]="\$module_handler->alter(\$type, &\$data, &\$context1 = NULL, &\$context2 = NULL)"
   [drupal_build_form]="\Drupal::formBuilder()->buildForm()"
   [drupal_clean_css_identifier]="use Drupal\Component\Utility\Html; / Html::cleanCssIdentifier($foo);"
+  [drupal_convert_to_utf8]="Unicode::convertToUtf8()"
   [drupal_deliver_html_page]="TODO"
   [drupal_deliver_page]="TODO"
   [drupal_encode_path]="UrlHelper::encodePath('drupal')"
@@ -67,11 +69,13 @@ FUNCTIONS=(
   [drupal_goto]="\$this->redirect(\$route_name)"
   [drupal_html_class]="use Drupal\Component\Utility\Html; / Html::getClass($foo);"
   [drupal_html_id]="use Drupal\Component\Utility\Html; / Html::getId($foo);"
+  [drupal_html_to_text]="MailManagerInterface::mail() method - \Drupal::service('plugin.manager.mail')->mail($module, $key, $to, $langcode);"
   [drupal_http_build_query]="use Drupal\Component\Utility\UrlHelper; / UrlHelper::buildQuery"
   [drupal_http_request]="Use the Guzzle HTTP client library"
   [drupal_is_cli]="if (PHP_SAPI === 'cli') { // Do stuff }"
   [drupal_json_decode]="use Drupal\Component\Serialization\Json; / \$data = Json::decode(\$json);"
   [drupal_json_encode]="use Drupal\Component\Serialization\Json; / \$json = Json::encode(\$data);"
+  [drupal_mail]="MailManagerInterface::mail() method - \Drupal::service('plugin.manager.mail')->mail($module, $key, $to, $langcode);"
   [drupal_match_path]="\Drupal\Core\Path\PathMatcherInterface::matchPath()"
   [drupal_page_header]="TODO"
   [drupal_parse_url]="TODO"
@@ -88,7 +92,15 @@ FUNCTIONS=(
   [drupal_session_start]="SessionManager: \$session_manager = \Drupal::service('session_manager') / \$session_manager->start()"
   [drupal_session_started]="SessionManager: \$session_manager = \Drupal::service('session_manager') / \$session_manager->isStarted()"
   [drupal_set_title]="Titles (static or dynamic) are now defined on routes - See https://www.drupal.org/node/2067859"
+  [drupal_strlen]="Unicode::strlen()"
+  [drupal_strtolower]="Unicode::strtolower()"
+  [drupal_strtoupper]="Unicode::strtoupper()"
+  [drupal_substr]="Unicode::substr()"
+  [drupal_truncate_bytes]="Unicode::truncateBytes()"
+  [drupal_ucfirst]="Unicode::ucfirst()"
   [drupal_validate_form]="\Drupal::formBuilder()->validateForm()"
+  [drupal_validate_utf8]="Unicode::validateUtf8()"
+  [drupal_wrap_mail]="MailManagerInterface::mail() method - \Drupal::service('plugin.manager.mail')->mail($module, $key, $to, $langcode);"
   [drupal_write_record]="Developers should use \Drupal::database()->merge(), the Entity API or one of the provided services, like key value"
   [element_child]="Element::child('foo')"
   [element_children]="Element::children(\$elements)"
@@ -117,11 +129,14 @@ FUNCTIONS=(
   [form_get_cache]="\Drupal::formBuilder()->getCache()"
   [form_get_error]="\Drupal::formBuilder()->getError()"
   [form_get_errors]="\Drupal::formBuilder()->getErrors()"
+  [form_load_include]="$form_state->loadInclude()"
   [form_options_flatten]="\$flattened_options = OptGroup::flattenOptions(\$options)"
   [form_set_cache]="\Drupal::formBuilder()->setCache()"
   [form_set_error]="\Drupal::formBuilder()->setErrorByName()"
   [form_set_value]="\Drupal::formBuilder()->setValue(\$element, \$value, \$form_state);"
   [form_state_defaults]="\Drupal::formBuilder()->getFormStateDefaults()"
+  [form_state_values_clean]="$form_state->cleanValues()"
+  [format_plural]="\Drupal::translation()->formatPlural() or \Drupal::service('date.formatter')->formatInterval() - See https://www.drupal.org/node/2173787"
   [get_t]="TODO"
   [hook_comment_publish]="See https://www.drupal.org/node/2296867"
   [hook_comment_unpublish]="See https://www.drupal.org/node/2296867"
@@ -192,8 +207,11 @@ FUNCTIONS=(
   [language_url_rewrite_session]="LanguageNegotiationSession::processOutbound"
   [list_themes]="\$theme_handler = \Drupal::service('theme_handler')"
   [menu_get_active_trail]="\Drupal::service('menu_link.tree')->getActiveTrailIds(\$menu_name)"
+  [menu_link_content_uninstall]=""
   [menu_set_active_item]="\Drupal::service('menu_link.tree')->getActiveTrailIds(\$menu_name)"
   [menu_set_active_trail]="\Drupal::service('menu_link.tree')->getActiveTrailIds(\$menu_name)"
+  [mime_header_decode]="Unicode::mimeHeaderDecode()"
+  [mime_header_encode]="Unicode::mimeHeaderEncode()"
   [module_exists]="\Drupal::moduleHandler()->moduleExists('module_name')"
   [module_invoke]="\$module_handler->invoke(\$module, \$hook, \$args = array())"
   [module_invoke_all]="\Drupal::moduleHandler()->invokeAll(\$hook, \$args = array())"
@@ -217,11 +235,14 @@ FUNCTIONS=(
   [theme_enable]="\$theme_handler->enable(\$theme_list)"
   [theme_disable]="\$theme_handler->disable(\$theme_list)"
   [theme_link]="#type' => 'link'"
+  [truncate_utf8]="Unicode::truncate()"
+  [unicode_check]="Unicode::check()"
   [url]="use Drupal\Core\Url; / \$internal_link = \$this->l(t('Book admin'), Url::fromRoute('book.admin')); / \$external_link = \$this->l(t('External link'), Url::fromUri('http://www.example.com/'));"
   [url_is_external]="UrlHelper::isExternal('http://www.drupal.org')"
   [user_access]="User::hasPermission()"
   [user_load_multiple]="\Drupal\user\Entity\User::loadMultiple()"
   [valid_url]="TODO"
+  [views_get_applicable_views]="TBD"
 )
 
 for API_FUNCTIONS in ${!FUNCTIONS[@]}; do
@@ -248,11 +269,32 @@ CONSTANTS=(
   [MENU_LINKS_TO_PARENT]="No replacement"
   [MENU_VISIBLE_IN_TREE]="No replacement"
   [MENU_IS_ROOT]="No replacement"
+  [PREG_CLASS_UNICODE_WORD_BOUNDARY]="Unicode::PREG_CLASS_WORD_BOUNDARY"
+  [UNICODE_ERROR]="Unicode::STATUS_ERROR"
+  [UNICODE_MULTIBYTE]="Unicode::STATUS_MULTIBYTE"
+  [UNICODE_SINGLEBYTE]="Unicode::STATUS_SINGLEBYTE"
   )
 
 for API_CONSTANTS in ${!CONSTANTS[@]}; do
   if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep "${API_CONSTANTS}") ]]; then
     echo -e "\tReplace ${RED}${API_CONSTANTS}()${COLOR_ENDING} by ${GREEN}${CONSTANTS[${API_CONSTANTS}]}${COLOR_ENDING}"
+  fi
+done
+
+################
+# GLOBALS #
+################
+echo -e "${BLUE}Auditing globals...${COLOR_ENDING}"
+
+declare -A GLOBALS
+GLOBALS=(
+  [\$user]="\Drupal::currentUser"
+  [\$multibyte]="Unicode::getStatus() / Unicode::setStatus($status)"
+)
+
+for API_GLOBALS in ${!GLOBALS[@]}; do
+  if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep "${API_GLOBALS}") ]]; then
+    echo -e "\tReplace ${RED}${API_GLOBALS}()${COLOR_ENDING} by ${GREEN}${GLOBALS[${API_GLOBALS}]}${COLOR_ENDING}"
   fi
 done
 
@@ -269,7 +311,7 @@ SUPERGLOBALS=(
 
 # Searching for $GET_['q'] doesn't work well as an array key. Hardcoding it for now.
 if [[ $(find ${VALID_PATH} -type f ! -name "*.css" ! -name "*.js" | xargs grep "\$_GET\['q']") ]]; then
-  echo -e "\tReplace ${RED}\$_GET['q']${COLOR_ENDING} by the ${GREEN}<current> route - See https://www.drupal.org/node/2382211${COLOR_ENDING}"
+  echo -e "\tReplace ${RED}\$_GET['q']${COLOR_ENDING} by ${GREEN}<current> route - See https://www.drupal.org/node/2382211${COLOR_ENDING}"
 fi
 
 for API_SGLOBALS in ${!SUPERGLOBALS[@]}; do
