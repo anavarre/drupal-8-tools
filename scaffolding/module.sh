18d87b0cbd57d48724c6625209725f8ab8fe397b
Aurelien Navarre <aurelien.navarre@gmail.com>
2015-09-08 13:46:43 +0200

    Fix indentation

--- scaffolding/module.sh
+++ scaffolding/module.sh
@@ -18,14 +18,14 @@ DESCRIPTION=$2
 PACKAGE=$3
 
 if [[ -z $1 ]] && [[ -z $2 ]] && [[ -z $3 ]]; then
-	echo -n "Module name (required): "
-	read NAME
-	
-	echo -n "Description (required): "
-	read DESCRIPTION
+  echo -n "Module name (required): "
+  read NAME
 
-	echo -n "Package (optional): "
-	read PACKAGE
+  echo -n "Description (required): "
+  read DESCRIPTION
+
+  echo -n "Package (optional): "
+  read PACKAGE
 fi
 
 # Confirming path.
@@ -55,11 +55,11 @@ NAME_LOWER="${NAME,,}"
 
 # Create module directory.
 if [[ -d ${NAME_LOWER} ]]; then
-	echo -e "${RED}${NAME_LOWER} directory already exists! Aborting...${COLOR_ENDING}"
-	exit 1
+  echo -e "${RED}${NAME_LOWER} directory already exists! Aborting...${COLOR_ENDING}"
+  exit 1
 else
-	echo -e "\tCreating ${NAME_LOWER} directory..."
-	mkdir ${NAME_LOWER}
+  echo -e "\tCreating ${NAME_LOWER} directory..."
+  mkdir ${NAME_LOWER}
 fi
 
 #######
@@ -68,11 +68,11 @@ fi
 
 # Create the config directory
 if [[ -d ${NAME_LOWER}/config ]]; then
-	echo -e "\t${BLUE}${NAME_LOWER}/config directory already exists! Skipping...${COLOR_ENDING}"
+  echo -e "\t${BLUE}${NAME_LOWER}/config directory already exists! Skipping...${COLOR_ENDING}"
 else
-	echo -e "\tCreating ${NAME_LOWER}/config directory..."
-	mkdir ${NAME_LOWER}/config
-	mkdir ${NAME_LOWER}/config/install
+  echo -e "\tCreating ${NAME_LOWER}/config directory..."
+  mkdir ${NAME_LOWER}/config
+  mkdir ${NAME_LOWER}/config/install
 fi
 
 # Offer to create a schema for the configuration files.
@@ -105,25 +105,25 @@ while true; do
     read -p "Create Controller scaffolding? [Y/N] " ANSWER
     case ${ANSWER} in
         [Nn] )
-			break;;
+      break;;
         [Yy] )
-			if [[ -d ${NAME_LOWER}/src ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src directory..."
-				mkdir ${NAME_LOWER}/src
-			fi
-		
-			if [[ -d ${NAME_LOWER}/src/Controller ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Controller directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Controller directory..."
-				mkdir ${NAME_LOWER}/src/Controller
-			fi
-		
-			# Ensure module's first letter is uppercase.
-			NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
-			touch ${NAME_LOWER}/src/Controller/${NAME_1ST_UP}Controller.php
+      if [[ -d ${NAME_LOWER}/src ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src directory..."
+        mkdir ${NAME_LOWER}/src
+      fi
+
+      if [[ -d ${NAME_LOWER}/src/Controller ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Controller directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Controller directory..."
+        mkdir ${NAME_LOWER}/src/Controller
+      fi
+
+      # Ensure module's first letter is uppercase.
+      NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
+      touch ${NAME_LOWER}/src/Controller/${NAME_1ST_UP}Controller.php
 
 # Generating ModuleController.php default values
 cat <<EOT >> ${NAME_LOWER}/src/Controller/${NAME_1ST_UP}Controller.php
@@ -137,10 +137,10 @@ cat <<EOT >> ${NAME_LOWER}/src/Controller/${NAME_1ST_UP}Controller.php
 namespace Drupal\\${NAME_LOWER}\Controller;
 EOT
 
-			echo -e "${GREEN}Successfully created Controller scaffolding!${COLOR_ENDING}"
-			break;;
+      echo -e "${GREEN}Successfully created Controller scaffolding!${COLOR_ENDING}"
+      break;;
         * )
-			echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
+      echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
     esac
 done
 
@@ -149,25 +149,25 @@ while true; do
     read -p "Create Access checking? [Y/N] " ANSWER
     case ${ANSWER} in
         [Nn] )
-			break;;
+      break;;
         [Yy] )
-			if [[ -d ${NAME_LOWER}/src ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src directory..."
-				mkdir ${NAME_LOWER}/src
-			fi
-		
-			if [[ -d ${NAME_LOWER}/src/Access ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Access directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Access directory..."
-				mkdir ${NAME_LOWER}/src/Access
-			fi
-		
-			# Ensure module's first letter is uppercase.
-			NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
-			touch ${NAME_LOWER}/src/Access/${NAME_1ST_UP}AccessCheck.php
+      if [[ -d ${NAME_LOWER}/src ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src directory..."
+        mkdir ${NAME_LOWER}/src
+      fi
+
+      if [[ -d ${NAME_LOWER}/src/Access ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Access directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Access directory..."
+        mkdir ${NAME_LOWER}/src/Access
+      fi
+
+      # Ensure module's first letter is uppercase.
+      NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
+      touch ${NAME_LOWER}/src/Access/${NAME_1ST_UP}AccessCheck.php
 
 # Generating ModuleAccessCheck.php default values
 cat <<EOT >> ${NAME_LOWER}/src/Access/${NAME_1ST_UP}AccessCheck.php
@@ -190,10 +190,10 @@ class ${NAME_1ST_UP}AccessCheck implements StaticAccessCheckInterface {
 }
 EOT
 
-			echo -e "${GREEN}Successfully created Access checking!${COLOR_ENDING}"
-			break;;
+      echo -e "${GREEN}Successfully created Access checking!${COLOR_ENDING}"
+      break;;
         * )
-			echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
+      echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
     esac
 done
 
@@ -202,29 +202,29 @@ while true; do
     read -p "Create Form config scaffolding? [Y/N] " ANSWER
     case ${ANSWER} in
         [Nn] )
-			break;;
+      break;;
         [Yy] )
-			if [[ -d ${NAME_LOWER}/src ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src directory..."
-				mkdir ${NAME_LOWER}/src
-			fi
+      if [[ -d ${NAME_LOWER}/src ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src directory..."
+        mkdir ${NAME_LOWER}/src
+      fi
 
-			if [[ -d ${NAME_LOWER}/src/Form ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Form directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Form directory..."
-				mkdir ${NAME_LOWER}/src/Form
-			fi
+      if [[ -d ${NAME_LOWER}/src/Form ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Form directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Form directory..."
+        mkdir ${NAME_LOWER}/src/Form
+      fi
 
-			# Ensure module's first letter is uppercase.
-			NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
-			touch ${NAME_LOWER}/src/Form/${NAME_1ST_UP}ConfigForm.php
-			echo -e "${GREEN}Successfully created Form config scaffolding!${COLOR_ENDING}"
-			break;;
+      # Ensure module's first letter is uppercase.
+      NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
+      touch ${NAME_LOWER}/src/Form/${NAME_1ST_UP}ConfigForm.php
+      echo -e "${GREEN}Successfully created Form config scaffolding!${COLOR_ENDING}"
+      break;;
         * )
-			echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
+      echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
     esac
 done
 
@@ -233,36 +233,36 @@ while true; do
     read -p "Create Block scaffolding? [Y/N] " ANSWER
     case ${ANSWER} in
         [Nn] )
-			break;;
+      break;;
         [Yy] )
-			if [[ -d ${NAME_LOWER}/src ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src directory..."
-				mkdir ${NAME_LOWER}/src
-			fi
+      if [[ -d ${NAME_LOWER}/src ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src directory..."
+        mkdir ${NAME_LOWER}/src
+      fi
 
-			if [[ -d ${NAME_LOWER}/src/Plugin ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Plugin directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Plugin directory..."
-				mkdir ${NAME_LOWER}/src/Plugin
-			fi
+      if [[ -d ${NAME_LOWER}/src/Plugin ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Plugin directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Plugin directory..."
+        mkdir ${NAME_LOWER}/src/Plugin
+      fi
 
-			if [[ -d ${NAME_LOWER}/src/Plugin/Block ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Plugin/Block directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Plugin/Block directory..."
-				mkdir ${NAME_LOWER}/src/Plugin/Block
-			fi
+      if [[ -d ${NAME_LOWER}/src/Plugin/Block ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Plugin/Block directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Plugin/Block directory..."
+        mkdir ${NAME_LOWER}/src/Plugin/Block
+      fi
 
-			# Ensure module's first letter is uppercase.
-			NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
-			touch ${NAME_LOWER}/src/Plugin/Block/${NAME_1ST_UP}Block.php
-			echo -e "${GREEN}Successfully created Block scaffolding!${COLOR_ENDING}"
-			break;;
+      # Ensure module's first letter is uppercase.
+      NAME_1ST_UP=`sed 's/\(.\)/\U\1/' <<< "${NAME_LOWER}"`
+      touch ${NAME_LOWER}/src/Plugin/Block/${NAME_1ST_UP}Block.php
+      echo -e "${GREEN}Successfully created Block scaffolding!${COLOR_ENDING}"
+      break;;
         * )
-			echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
+      echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
     esac
 done
 
@@ -271,26 +271,26 @@ while true; do
     read -p "Create SimpleTest scaffolding? [Y/N] " ANSWER
     case ${ANSWER} in
         [Nn] )
-			break;;
+      break;;
         [Yy] )
-			if [[ -d ${NAME_LOWER}/src ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src directory..."
-				mkdir ${NAME_LOWER}/src
-			fi
+      if [[ -d ${NAME_LOWER}/src ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src directory..."
+        mkdir ${NAME_LOWER}/src
+      fi
 
-			if [[ -d ${NAME_LOWER}/src/Tests ]]; then
-				echo -e "\t${BLUE}${NAME_LOWER}/src/Tests directory already exists! Skipping...${COLOR_ENDING}"
-			else
-				echo -e "\tCreating ${NAME_LOWER}/src/Tests directory..."
-				mkdir ${NAME_LOWER}/src/Tests
-			fi
+      if [[ -d ${NAME_LOWER}/src/Tests ]]; then
+        echo -e "\t${BLUE}${NAME_LOWER}/src/Tests directory already exists! Skipping...${COLOR_ENDING}"
+      else
+        echo -e "\tCreating ${NAME_LOWER}/src/Tests directory..."
+        mkdir ${NAME_LOWER}/src/Tests
+      fi
 
-			echo -e "${GREEN}Successfully created Tests scaffolding!${COLOR_ENDING}"
-			break;;
+      echo -e "${GREEN}Successfully created Tests scaffolding!${COLOR_ENDING}"
+      break;;
         * )
-			echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
+      echo -e "${RED}Sorry, you need to answer by Y/y (yes) or N/n (no).${COLOR_ENDING}"
     esac
 done
 
@@ -298,8 +298,8 @@ done
 # Default files #
 #################
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.info.yml ]]; then
-	echo -e "\tCreating ${NAME}.info.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.info.yml
+  echo -e "\tCreating ${NAME}.info.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.info.yml
 
 # Generating info.yml default values
 cat <<EOT >> ${NAME_LOWER}/${NAME_LOWER}.info.yml
@@ -309,40 +309,40 @@ description: '${DESCRIPTION}.'
 core: 8.x
 EOT
 
-	# Only add package if any was entered.
-	if [[ ! -z ${PACKAGE} ]]; then
-		sed -i "4ipackage: ${PACKAGE}" ${NAME_LOWER}/${NAME_LOWER}.info.yml
-	fi
+  # Only add package if any was entered.
+  if [[ ! -z ${PACKAGE} ]]; then
+    sed -i "4ipackage: ${PACKAGE}" ${NAME_LOWER}/${NAME_LOWER}.info.yml
+  fi
 fi
 
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.module ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.module..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.module
-	echo "<?php" >> ${NAME_LOWER}/${NAME_LOWER}.module
+  echo -e "\tCreating ${NAME_LOWER}.module..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.module
+  echo "<?php" >> ${NAME_LOWER}/${NAME_LOWER}.module
 fi
 
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.routing.yml ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.routing.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.routing.yml
+  echo -e "\tCreating ${NAME_LOWER}.routing.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.routing.yml
 fi
 
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.links.menu.yml ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.links.menu.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.links.menu.yml
+  echo -e "\tCreating ${NAME_LOWER}.links.menu.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.links.menu.yml
 fi
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.links.task.yml ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.links.task.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.links.task.yml
+  echo -e "\tCreating ${NAME_LOWER}.links.task.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.links.task.yml
 fi
 
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.links.action.yml ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.links.action.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.links.action.yml
+  echo -e "\tCreating ${NAME_LOWER}.links.action.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.links.action.yml
 fi
 
 if [[ ! -f ${NAME_LOWER}/${NAME_LOWER}.services.yml ]]; then
-	echo -e "\tCreating ${NAME_LOWER}.services.yml..."
-	touch ${NAME_LOWER}/${NAME_LOWER}.services.yml
+  echo -e "\tCreating ${NAME_LOWER}.services.yml..."
+  touch ${NAME_LOWER}/${NAME_LOWER}.services.yml
 fi
 
 echo -e "${GREEN}Successfully generated module scaffolding!${COLOR_ENDING}"
