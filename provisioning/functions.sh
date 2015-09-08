beebe37dc27c08af5bdcc415828fe5cf72eae3f7
Aurelien Navarre <aurelien.navarre@gmail.com>
2015-09-08 11:57:04 +0200

    Improve root detection

--- provisioning/functions.sh
+++ provisioning/functions.sh
@@ -1,10 +1,7 @@
-################
-# REQUIREMENTS #
-################
-
+# Shared functions
 is_root() {
   # Make sure only root can execute the script
-  if [[ "$(whoami)" != "root" ]]; then
+  if [[ ! $(id -u) = 0 ]]; then
     echo -e "${RED}You are required to run this script as root or with sudo! Aborting...${COLOR_ENDING}"
     exit 1
   fi
